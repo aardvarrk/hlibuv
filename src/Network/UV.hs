c737f929e6ae1e40bb63cacb3a09e76365d366a9
Radek Slupik <daknok@icloud.com>
2012-11-27 15:13:29 +0100

    Add defaultLoop, createLoop and run

--- src/Network/UV.hs
+++ src/Network/UV.hs
@@ -1,3 +1,34 @@
+{-# LANGUAGE ForeignFunctionInterface #-}
 module Network.UV
-(
+( Loop
+, defaultLoop
+, createLoop
+, run
 ) where
+
+import           Foreign.C
+import           Foreign.Ptr
+
+foreign import ccall unsafe "uv_loop_new" c_uv_loop_new :: IO (Ptr a)
+foreign import ccall unsafe "uv_default_loop" c_uv_default_loop :: IO (Ptr a)
+foreign import ccall unsafe "uv_run" c_uv_run :: Ptr a -> IO CInt
+
+data Loop = Loop (Ptr CInt)
+
+-- | Get the default loop.
+--
+-- This function always returns the same loop.
+defaultLoop :: IO Loop
+defaultLoop = do
+    ptr <- c_uv_default_loop
+    return $ Loop ptr
+
+-- | Create a new loop.
+createLoop :: IO Loop
+createLoop = do
+    ptr <- c_uv_loop_new
+    return $ Loop ptr
+
+-- | Run the given loop.
+run :: Loop -> IO ()
+run (Loop ptr) = c_uv_run ptr >> return ()
